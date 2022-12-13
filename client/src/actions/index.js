27b2a045938d24e029bec1addb376cec2d59142b
FerMortaro <mortarofernanda@gmail.com>
2022-12-12 21:54:55 -0300

    fix

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,120 +1,115 @@
 import axios from "axios";
 export const GET_ALL_DOGS = "GET_ALL_DOGS";
 export const GET_TEMPERAMENT = "GET_TEMPERAMENT";
-export const FILTER_BY_TEMPERAMENT = "FILTER_BY_TEMPERAMENT"
-export const FILTER_BY_CREATED = "FILTER_BY_CREATED"
-export const ORDER_BY_NAME = "ORDER_BY_NAME"
-export const GET_DOG_NAME = "GET_DOG_NAME"
-export const POST_DOG = "POST_DOG"
-export const ORDER_BY_WEIGHT = "ORDER_BY_WEIGHT"
-export const GET_DETAIL = "GET_DETAIL"
-export const GET_CLEAN = "GET_CLEAN"
-export const SET_LOADING = "SET_LOADING"
-export const ERROR = "ERROR"
-export const DELETE_DOG = "DELETE_DOG"
-
-
+export const FILTER_BY_TEMPERAMENT = "FILTER_BY_TEMPERAMENT";
+export const FILTER_BY_CREATED = "FILTER_BY_CREATED";
+export const ORDER_BY_NAME = "ORDER_BY_NAME";
+export const GET_DOG_NAME = "GET_DOG_NAME";
+export const POST_DOG = "POST_DOG";
+export const ORDER_BY_WEIGHT = "ORDER_BY_WEIGHT";
+export const GET_DETAIL = "GET_DETAIL";
+export const GET_CLEAN = "GET_CLEAN";
+export const SET_LOADING = "SET_LOADING";
+export const ERROR = "ERROR";
+export const DELETE_DOG = "DELETE_DOG";
 
 export function getAllDogs() {
   return async function (dispatch) {
     let dogs = await axios.get("http://localhost:3001/dogs");
     return dispatch({
-        type: GET_ALL_DOGS,
-        payload: dogs.data  //.origin
-    })
-  }
+      type: GET_ALL_DOGS,
+      payload: dogs.data, //.origin
+    });
+  };
 }
-
 
 export function getTemperament() {
-  return async function(dispatch){
-    let temp = await axios.get("http://localhost:3001/temperaments")
+  return async function (dispatch) {
+    let temp = await axios.get("http://localhost:3001/temperaments");
     return dispatch({
       type: GET_TEMPERAMENT,
-      payload: temp.data
-    })
-  }
+      payload: temp.data,
+    });
+  };
 }
 
-export function postDog(payload){ //toda la data para crear el dog
-  return async function(dispatch){
-    let resp = await axios.post("http://localhost:3001/dogs" , payload)
-      console.log(resp)
-      return resp
-  }
+export function postDog(payload) {
+  //toda la data para crear el dog
+  return async function (dispatch) {
+    let resp = await axios.post("http://localhost:3001/dogs", payload);
+    return resp;
+  };
 }
 
 export function getDogName(name) {
-  console.log(name)
+  console.log(name);
   return async function (dispatch) {
     try {
-      let dogName = await axios.get(`http://localhost:3001/dogs?name=${name}`)
+      let dogName = await axios.get(`http://localhost:3001/dogs?name=${name}`);
       return dispatch({
         type: GET_DOG_NAME,
-        payload: dogName.data 
-      })
-      
+        payload: dogName.data,
+      });
     } catch (error) {
-     alert('Dog Not Found')
+      alert("Dog Not Found");
     }
-  }
+  };
 }
 
-export function filterDogsbyTemperament(payload){
-  console.log(payload)
+export function filterDogsbyTemperament(payload) {
+  console.log(payload);
   return {
-      type: FILTER_BY_TEMPERAMENT,
-      payload
-  }
-};
+    type: FILTER_BY_TEMPERAMENT,
+    payload,
+  };
+}
 
 export function filterCreated(payload) {
-  return{
+  return {
     type: FILTER_BY_CREATED,
-    payload
-  }
+    payload,
+  };
 }
 
 export function orderByName(payload) {
-  console.log(payload)
+  console.log(payload);
   return {
     type: ORDER_BY_NAME,
-    payload
-  }
-  
+    payload,
+  };
 }
 
-export function orderByWeight(payload){
+export function orderByWeight(payload) {
   return {
     type: ORDER_BY_WEIGHT,
-    payload
-  }
+    payload,
+  };
 }
 
 export function getDetail(id) {
-  return async function(dispatch) {
+  return async function (dispatch) {
     try {
-      let info = await axios.get(`http://localhost:3001/dogs/${id}`)
+      let info = await axios.get(`http://localhost:3001/dogs/${id}`);
       return dispatch({
         type: GET_DETAIL,
-        payload: info.data
-      })
+        payload: info.data,
+      });
     } catch (error) {
-      alert(error) //PROBAR!!
+      alert(error); //PROBAR!!
     }
-  }
+  };
 }
 
 export function getClean() {
-  return{
-      type: GET_CLEAN,
-      payload: []
-  }
+  return {
+    type: GET_CLEAN,
+    payload: [],
+  };
 }
 
 // export function setLoading() {
 //   return {
-//      type: SET_LOADING 
+//      type: SET_LOADING
 //     };
 // };
 
@@ -144,7 +139,7 @@ export function getClean() {
 //     await axios.delete(`http://localhost:3001/delete/${id}`);
 //     return dispatch({
 //         type: DELETE_DOG,
-        
+
 //     })
 //   }
 // }
