3a7a1afa00b098ec32f4ad99bac7fda2c90d7bcc
FerMortaro <mortarofernanda@gmail.com>
2022-12-15 13:27:26 -0300

    fix dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -5,15 +5,12 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./store";
 import { Provider } from "react-redux";
-import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.render(
   <Provider store={store}>
-     <ChakraProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-    </ChakraProvider>
   </Provider>,
 
   document.getElementById("root")
